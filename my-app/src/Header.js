6ff393f1e6ab9a00cd03c321fca2c736644a8306
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-08-26 23:47:06 +0530

    Inline style

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 import styles from './App.module.css';
 
+const size = {
+    fontSize: '50px',
+    color: 'red'
+}
+
 function Header(props){
     const {name, desc} = props;
     function clickMe(){
@@ -10,7 +15,7 @@ function Header(props){
     return <div className="App-header">
         <h1 className={styles.backGroundColor}>{desc}Welcome to NQ Media</h1>
         <h1>{name}Other header</h1>
-        <span>Home</span> <span>Services</span> <span><small>NQ Prime</small></span> <span>Contact us</span>
+        <span>Home</span> <span>Services</span> <span style={size}><small>NQ Prime</small></span> <span>Contact us</span>
         <button onClick={clickMe}>Click this button</button>
     </div>
 }
