b63c824c906dae8b0009bc5f612e42b65331df32
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-08-30 08:28:27 +0530

    Minor changes

--- my-app/src/Form.js
+++ my-app/src/Form.js
@@ -3,7 +3,11 @@ import React, {Component} from 'react';
 class Form extends Component{
     render(){
         return <div>
+            <label for="">First Name:</label>
             <input type="text"></input>
+
         </div>
     }
 }
+
+export default Form;
