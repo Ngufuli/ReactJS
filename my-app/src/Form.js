f0efe0df8b7d112672dad4d0d59a2fcc5c01d498
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-08-30 08:35:31 +0530

    Minor changes

--- my-app/src/Form.js
+++ my-app/src/Form.js
@@ -3,10 +3,15 @@ import React, {Component} from 'react';
 class Form extends Component{
     render(){
         return <div>
+            <br />
             <label for="">First Name:</label>
             <input type="text"></input>
+            <br />
             <label for="">Last Name:</label>
             <input type="text"></input>
+            <br />
+            <label for="">Password</label>
+            <input type="password"></input>
 
         </div>
     }
