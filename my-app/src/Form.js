495ad34f27b7ce32cf11bb7d0ef92bce724d912e
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-08-30 08:40:59 +0530

    Wrapped up content into the form in Form component

--- my-app/src/Form.js
+++ my-app/src/Form.js
@@ -4,16 +4,18 @@ class Form extends Component{
     render(){
         return <div>
             <br />
-            <label for="">First Name:</label>
-            <input type="text"></input>
-            <br />
-            <label for="">Last Name:</label>
-            <input type="text"></input>
-            <br />
-            <label for="">Password</label>
-            <input type="password"></input>
-            <br />
-            {/* <input type="textarea" width="250px" height="250px"></input> */}
+            <form>
+                <label for="">First Name:</label>
+                <input type="text"></input>
+                <br />
+                <label for="">Last Name:</label>
+                <input type="text"></input>
+                <br />
+                <label for="">Password</label>
+                <input type="password"></input>
+                <br />
+                {/* <input type="textarea" width="250px" height="250px"></input> */}
+            </form>
         </div>
     }
 }
