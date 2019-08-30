9283ee3134363d7fa25c440dacc736a4b68461d5
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-08-30 08:38:03 +0530

    Minor changes

--- my-app/src/Form.js
+++ my-app/src/Form.js
@@ -12,7 +12,8 @@ class Form extends Component{
             <br />
             <label for="">Password</label>
             <input type="password"></input>
-
+            <br />
+            <input type="textarea" width="250px" height="250px"></input>
         </div>
     }
 }
