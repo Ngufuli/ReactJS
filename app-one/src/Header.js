241ae7dde2a629ccd7bd0bf4990e1fc2445a302c
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-09-07 18:35:15 +0530

    Added header

---
+++ app-one/src/Header.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Header extends Component{
+    state = {
+
+    }
+
+    render(){
+        return <div>
+            <h2>This is the header section</h2>
+        </div>
+    }
+}
+
+export default Header;
