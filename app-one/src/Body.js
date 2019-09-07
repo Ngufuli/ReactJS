4334bd50fd5fe1f3563e49bc28aa5538811bc863
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-09-08 01:11:50 +0530

    Functional component

--- app-one/src/Body.js
+++ app-one/src/Body.js
@@ -1,21 +1,39 @@
+// import React, {Component} from 'react';
+
+// class Body extends Component{
+//     state = {
+
+//     }
+
+//     render(){
+//         return<div>
+//             <ol>
+//                 <li>Moja</li>
+//                 <li>Mbili</li>
+//                 <li>Tatu</li>
+//                 <li>Nne</li>
+//                 <li>Tano</li>
+//             </ol>
+//         </div>
+//     }
+// }
+
+// export default Body;
+
+//======================FUNCTIONAL COMPONENTS============================================
+
 import React, {Component} from 'react';
 
-class Body extends Component{
-    state = {
-
-    }
-
-    render(){
-        return<div>
-            <ol>
-                <li>Moja</li>
-                <li>Mbili</li>
-                <li>Tatu</li>
-                <li>Nne</li>
-                <li>Tano</li>
-            </ol>
-        </div>
-    }
+function Body(){
+    return <div>
+        <ol>
+            <li>Moja</li>
+            <li>Mbili</li>
+            <li>Tatu</li>
+            <li>Nne</li>
+            <li>Tano</li>
+        </ol>
+    </div>
 }
 
 export default Body;
