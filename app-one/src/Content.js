e3136544e82547693ae2a665f199ba5146108e7a
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-09-12 00:52:05 +0530

    props

--- app-one/src/Content.js
+++ app-one/src/Content.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 
 function Content(props){
+    console.log(props);
 const    styles = {
         width: '150px',
         height: '125px',
@@ -12,9 +13,10 @@ const    styles = {
     }
     return(
         <div>
-            <img style={styles} src="https://images.unsplash.com/photo-1499084732479-de2c02d45fcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
-            <p>Phone: +91 9115490856</p>
-            <p>email: nq@navquery.com</p>
+            <h4>{props.name}</h4>
+            <img src={props.imageUrl} />
+            <p>Phone: {props.phone}</p>
+            <p>email: {props.email}</p>
             {/* <img style={styles} src="https://images.unsplash.com/photo-1495344517868-8ebaf0a2044a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/> */}
         </div>
     );
