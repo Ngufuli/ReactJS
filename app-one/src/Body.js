061fad1173f0e6982777b447fd9e4b23d9223538
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-09-11 21:07:14 +0530

    styles

--- app-one/src/Body.js
+++ app-one/src/Body.js
@@ -46,7 +46,7 @@ function Body(){
     return(
     <div>
     <h2 style={{color: '#aaf'}}>{timing}</h2>
-    <h3 style={{color: '#faf'}}>Hellow Mr. {firstName + " " + lastName}</h3>
+    <h3 style={{color: '#faf', backgroundColor: "#ccf"}}>Hellow Mr. {firstName + " " + lastName}</h3>
     <h3>{`Collage: ${collage}`}</h3>
     <h3>{`Degree: ${degree}`}</h3>
     </div>
