1f325737e59f059ac6bfa46ca97d837873d3e88b
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-09-15 10:57:49 +0530

    Babel/watch/babel cli/babel preset

---
+++ frm_bks/static/app.jsx
@@ -0,0 +1,7 @@
+const contentNode = document.getElementById('contents');
+
+const continents = ['Africa', 'America', 'Asia','Australia','Europe'];
+const message = continents.map(c => `Hello ${c}!`).join(' ');
+
+const component = <p>{message}</p>
+ReactDOM.render(component, contentNode);
